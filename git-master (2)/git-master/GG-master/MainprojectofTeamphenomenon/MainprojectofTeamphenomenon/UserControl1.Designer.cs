a2b405d52c4f9fca9bb49e57a901444181859d92
Satpainglwin <67333667+Satpainglwin@users.noreply.github.com>
2020-09-01 11:24:55 +0630

    Add files via upload

---
+++ git-master (2)/git-master/GG-master/MainprojectofTeamphenomenon/MainprojectofTeamphenomenon/UserControl1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace MainprojectofTeamphenomenon
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.splitter3 = new System.Windows.Forms.Splitter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.Color.Red;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(344, 449);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            this.splitter1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitter1_SplitterMoved);
+            // 
+            // splitter2
+            // 
+            this.splitter2.BackColor = System.Drawing.Color.Yellow;
+            this.splitter2.Location = new System.Drawing.Point(344, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(350, 449);
+            this.splitter2.TabIndex = 1;
+            this.splitter2.TabStop = false;
+            // 
+            // splitter3
+            // 
+            this.splitter3.BackColor = System.Drawing.Color.Lime;
+            this.splitter3.Location = new System.Drawing.Point(694, 0);
+            this.splitter3.Name = "splitter3";
+            this.splitter3.Size = new System.Drawing.Size(358, 449);
+            this.splitter3.TabIndex = 2;
+            this.splitter3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NEEDS TO IMPROVE";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(99, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Rate >>";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(458, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IT\'S OKAY";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(464, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 52);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Rate >>";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(854, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 32);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "GOOD";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(842, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 52);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Rate >>";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Cyan;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1185, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 32);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Amazing!!";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Cyan;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(1185, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 52);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Rate >>";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitter3);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.splitter1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1448, 449);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Splitter splitter3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ git-master (2)/git-master/GG-master/MainprojectofTeamphenomenon/MainprojectofTeamphenomenon/UserControl1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace MainprojectofTeamphenomenon
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-COH8Q90;Initial Catalog=realmenu;Persist Security Info=True;User ID=sa;Password=APO gaming");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into tblrating([Good],[Amazing!!],[Needs To Improve],[It's Okay])values('Rated','...','...','...')", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Thanks for rating");
+            con.Close();
+        }
+
+        private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-COH8Q90;Initial Catalog=realmenu;Persist Security Info=True;User ID=sa;Password=APO gaming");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into tblrating([Needs To Improve],[It's Okay],[Good],[Amazing!!])values('Rated','...','...','...')", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("We will try better at next time ");
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-COH8Q90;Initial Catalog=realmenu;Persist Security Info=True;User ID=sa;Password=APO gaming");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into tblrating([It's Okay],[Needs To Improve],[Good],[Amazing!!])values('Rated','...','...','...')", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Thanks for rating ");
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-COH8Q90;Initial Catalog=realmenu;Persist Security Info=True;User ID=sa;Password=APO gaming");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into tblrating([Amazing!!],[It's Okay],[Needs To Improve],[Good])values('Rated','...','...','...')", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Yes, you are amazing !!");
+            con.Close();
+        }
+    }
+    }
+
+
